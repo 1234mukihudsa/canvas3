4717918cd2a485977602ff847bd9bcaa78370d17
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2022-04-10 08:51:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+diffrence = 0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,550);
@@ -13,8 +18,23 @@ function modelLoaded(){
 function gotPoses(results){
     if( results.length  >0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " nose y = "+ noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = floor(leftWristX - rightWristX);
+        console.log("leftwristx = " + leftWristX + "rightwristx = "+ rightWristX + "diffrence  = "+ diffrence);
+
+
     }
 }
 function draw(){
-    background("#87CEEB")
+    background("#87CEEB");
+    document.getElementById("square_size").innerHTML= "Width and height of the tex willbe -" + diffrence + "px";
+    textSize(diffrence);
+    text("MUkundan",100,
+    40
+    );
+    fill('#F90093');
 }
